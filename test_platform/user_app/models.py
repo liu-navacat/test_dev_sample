8d11b8bc26e88be875c4a6632aebe04b06ffc46a
llbhome <568947880@qq.com>
2020-12-31 15:47:25 +0800

    新增页面登录及前端页面样式

---
+++ test_platform/user_app/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+# ORM 操作数据库表的
+# select * from table_name
+# table_name.objects.all()
---
+++ test_platform/user_app/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib import auth
+# Create your views here.
+# 主要代码逻辑放在这里
+
+def index(request):
+    return render(request,"index.html")
+
+#处理登录请求#
+
+def login_action(request):
+    if request.method == "POST":
+        username = request.POST.get("username","")
+        password = request.POST.get("password","")
+
+        if username == "" or password == "":
+            return render(request, "index.html",
+                          {"error":"用户名或密码为空"})
+        else:
+            user = auth.authenticate(username = username,password = password)
+            print(user)
+            print(type(user))
+            if user is not None:
+                auth.login(request,user) #验证登录
+                return render(request, "project_manage.html")
+            else:
+                return render(request, "index.html",
+                          {"error": "用户名或密码错误"})
+
+
